0b952042cd60477b9ce6029ea2fb77e20aacc10b
gras64 <gras64@web.de>
2019-12-26 19:18:01 +0100

    bug fix

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
 from mycroft import MycroftSkill, intent_file_handler
 from lingua_franca.parse import extract_numbers, normalize
+from mycroft.util.log import LOG, getLogger
+import re
 
 
 class NavigationExample(MycroftSkill):
@@ -8,12 +10,10 @@ class NavigationExample(MycroftSkill):
 
     def initialize(self):
         self.navigation_active = True
-        self.settings["homecity"] = self.settings.get('homecity', "London")
-        self.settings["homestreet"] = self.settings.get('homestreet', "Winkelgasse")
-        self.settings["homenumber"] = self.settings.get('homenumber', "1")
         self.street = None
         self.city = None
         self.street_number = None
+        self.voice_proxy
         #self.navigation_active = self.is_navigation()
         self.register_intent_file('home.intent', self.home)
         self.register_intent_file('alternative.route.intent', self.alternative_route)
@@ -25,17 +25,24 @@ class NavigationExample(MycroftSkill):
     def navigation(self, message):
         self.city = message.data.get("city", None)
         self.street = message.data.get("street", None)
-        self.street_number = message.data.get("street_number", "1")
+        self.street_number = message.data.get("number", None)
         lang = self.lang
-        extract_number = None
-        try:
-            extract_number = extract_numbers(self.street, short_scale=False, ordinals=False,
-                                lang="en-us")
-            if not extract_number is None:
-                self.street_number = extract_number
-        except:
-            pass
+        if self.street is None:
+            self.street = self.get_response('witch.street', data={"city": self.city,})
+        #self.log.info('nummber: ' + str(self.street_number))
+        #self.log.info('street: ' + str(self.street))
+        if self.street_number is None:
+            extract_number = re.findall(r'\d+', self.street)
+            self.street_number = ' '.join(extract_number)
+            self.log.info('House nummber: ' + str(extract_number))
+            self.street = self.street.replace(self.street_number, '')
+        if not self.street_number.isdigit():
+            self.street_number = self.get_response('witch.number', data={"street": self.street})
         self.speak_dialog("route", data={"city": self.city, "street": self.street, "number": self.street_number})
+        self.schedule_repeating_event(self.voice_proxy, None, 1,
+                                          name='navigation_voice_proxy')
+        self.navigation_active = True
+
 
     def alternative_route(self, message):
         route = None
@@ -49,13 +56,18 @@ class NavigationExample(MycroftSkill):
 
     def go_home(self, message):
         self.speak_dialog('arrival')
+        self.cancel_scheduled_event('navigation_voice_proxy')
+        self.navigation_active = False
 
     def home(self, message):
-        self.city = self.settings["homecity"]
-        self.street = self.settings["homestreet"]
-        self.street_number = self.settings["homenumber"]
+        self.city = self.settings.get('homecity', "London")
+        self.street = self.settings.get('homestreet', "Winkelgasse")
+        self.street_number = self.settings.get('homenumber', "1")
         self.navigation_active = True
         self.speak_dialog('home')
+        self.navigation_active = True
+        self.schedule_repeating_event(self.voice_proxy, None, 1,
+                                          name='navigation_voice_proxy')
 
 
     def how_far(self, message):
@@ -79,10 +91,18 @@ class NavigationExample(MycroftSkill):
         if self.navigation_active is True:
             return True
         else:
+            self.cancel_scheduled_event('navigation_voice_proxy')
             return False
+
+    def voice_proxy(self):
+        self.log.info("check for navigation commands")  
+        #if command:
+        #    self.speak(command)
 
     def shutdown(self):
         super(NavigationExample, self).shutdown()
+        self.cancel_scheduled_event('navigation_voice_proxy')
+        self.settings.update
 
 def create_skill():
     return NavigationExample()
